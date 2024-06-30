637df1f4df422f4ad1310198e19150b11bcf009c
max burger <mburger89@me.com>
2024-06-30 00:46:33 -0700

    updates version of macos needed

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "pullList-api",
     platforms: [
-       .macOS(.v13)
+       .macOS(.v14)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
